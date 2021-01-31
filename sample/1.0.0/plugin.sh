549cf88f7afd094291c57f12b69ac12e306613ff
Simon Schmid <mail@sschmid.com>
2021-01-31 02:05:08 +0100

    Update sample sha

--- sample/1.0.0/plugin.sh
+++ sample/1.0.0/plugin.sh
@@ -7,6 +7,6 @@ BEE_PLUGIN_INFO="A template for bee plugins - https://github.com/sschmid/bee"
 BEE_PLUGIN_SOURCE_HTTPS="https://github.com/sschmid/bee-sample.git"
 BEE_PLUGIN_SOURCE_SSH="git@github.com:sschmid/bee-sample.git"
 BEE_PLUGIN_TAG="1.0.0"
-BEE_PLUGIN_SHA256="c3a21f6d6de3614ed2470f6e85491167aebebf6bb25426664849f63cbef7fbfa"
+BEE_PLUGIN_SHA256="c568f3d922e0370309ce30e00eef47392eb5ba9078e69ecd6bd43b26f54ba0f0"
 # Uncomment if this plugin has dependencies
 # BEE_PLUGIN_DEPENDENCIES=(android ios version)
